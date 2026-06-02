b2cfd78bb86e22646fc7012e630bab85b8430b0d
RenkiConnected <coriolis.saintraphael@gmail.com>
2026-06-02 21:48:11 +0200

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHACl2J65Z9DytRFxW0XdDOFZlX4jjjJ0",
+  authDomain: "renki-reparation-6a172.firebaseapp.com",
+  projectId: "renki-reparation-6a172",
+  storageBucket: "renki-reparation-6a172.firebasestorage.app",
+  messagingSenderId: "564905125449",
+  appId: "1:564905125449:web:b088ed8015763b1d850198"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
